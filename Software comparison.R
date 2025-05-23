9b232377ddeab2591b940ced416b86d5ef0f8eba
Tuo Lin <43549569+tuolin123@users.noreply.github.com>
2025-05-23 15:25:12 -0400

    Update

--- Software comparison.R
+++ Software comparison.R
@@ -5,14 +5,14 @@ library(swdpwr)
 
 #############################    Scenario 1      ###############################  
 # swCRTdesign
-swPwr(swDsn(c(1,1)), distn="gaussian",
-      n=18, mu0=1, mu1=1.5, sigma=1, tau=sqrt(1/99), 
+swPwr(swDsn(c(5,5)), distn="gaussian",
+      n=15, mu0=1, mu1=1.5, sigma=1, tau=sqrt(1/99), 
       eta=0, rho=0, gamma=0, alpha=0.05)
 
-completedesign <- matrix(c(c(0,1,1),c(0,0,1)),2,3,byrow=TRUE)
+completedesign <- matrix(c(rep(c(0,1,1),5),rep(c(0,0,1),5)),10,3,byrow=TRUE)
 
 # swdpower with time effect
-swdpower(K = 18, design = completedesign,
+swdpower(K = 15, design = completedesign,
          family = "gaussian", model = "marginal",
          link = "identity", type = "cross-sectional",
          meanresponse_start = 1,
@@ -22,7 +22,7 @@ swdpower(K = 18, design = completedesign,
          typeIerror = 0.05, alpha0 = 0.01, alpha1 = 0.01)
 
 # swdpower without time effect
-swdpower(K = 18, design = completedesign,
+swdpower(K = 15, design = completedesign,
          family = "gaussian", model = "marginal",
          link = "identity", type = "cross-sectional",
          meanresponse_start = 1,
@@ -30,15 +30,15 @@ swdpower(K = 18, design = completedesign,
          sigma = 1,
          typeIerror = 0.05, alpha0 = 0.01, alpha1 = 0.01)
 
-# PASS 0.294
-# R Shiny 0.293
+# PASS 0.826
+# R Shiny 0.829
 
 #############################    Scenario 2      ###############################
-swPwr(swDsn(c(1,1)), distn="binomial", 
-      n=50, mu0=0.1, mu1=0.3, icc = 0.01, 
+swPwr(swDsn(c(5,5)), distn="binomial", 
+      n=50, mu0=0.1, mu1=0.2, icc = 0.01, 
       alpha=0.05)
 
-completedesign <- matrix(c(c(0,1,1),c(0,0,1)),2,3,byrow=TRUE)
+completedesign <- matrix(c(rep(c(0,1,1),5),rep(c(0,0,1),5)),10,3,byrow=TRUE)
 
 # swdpower with time effect (For binary outcome, we can only do with time effect with small meanresponse_end0)
 swdpower(K = 50, design = completedesign,
@@ -46,7 +46,7 @@ swdpower(K = 50, design = completedesign,
          link = "identity", type = "cross-sectional",
          meanresponse_start = 0.1,
          meanresponse_end0 = 0.1001, # we can only specify a very small incremental of 0.1 to start for including time effect
-         meanresponse_end1 = 0.3,
+         meanresponse_end1 = 0.2,
          typeIerror = 0.05, alpha0 = 0.01, alpha1 = 0.01)
 
 
@@ -56,11 +56,11 @@ swdpower(K = 50, design = completedesign,
          link = "identity", type = "cross-sectional",
          meanresponse_start = 0.1,
          meanresponse_end0 = 0.1,
-         meanresponse_end1 = 0.3,
+         meanresponse_end1 = 0.2,
          typeIerror = 0.05, alpha0 = 0.01, alpha1 = 0.01)
 
-## PASS 0.64
-## R shiny 0.64
+## PASS 0.811
+## R shiny 0.811
 
 #############################    Scenario 3      ###############################
 swPwr(swDsn(c(5,5,5)), distn="binomial",
@@ -90,11 +90,11 @@ swdpower(K = 50, design = completedesign,
 
 #############################    Scenario 4      ###############################
 ## Batched design (separate time model)
-swPwr(swDsn(c(2,2)), distn="binomial", 
-      n=50, mu0=0.1, mu1=0.3, icc = 0.01, 
+swPwr(swDsn(c(6,6)), distn="binomial", 
+      n=50, mu0=0.1, mu1=0.2, icc = 0.01, 
       alpha=0.05)
 
-batchdesign <- matrix(c(c(0,1,1),c(0,0,1)),4,3,byrow=TRUE)
+batchdesign <- matrix(c(c(0,1,1),c(0,0,1)),12,3,byrow=TRUE)
 
 
 # swdpower with time effect (For binary outcome, we can only do with time effect with small meanresponse_end0)
@@ -103,15 +103,15 @@ swdpower(K = 50, design = batchdesign,
          link = "identity", type = "cross-sectional",
          meanresponse_start = 0.1,
          meanresponse_end0 = 0.1001, # we can only specify a very close number to start for including time effect
-         meanresponse_end1 = 0.3,
+         meanresponse_end1 = 0.2,
          typeIerror = 0.05, alpha0 = 0.01, alpha1 = 0.01)
 
 swdpower(K = 50, design = batchdesign,
          family = "binomial", model = "marginal",
          link = "identity", type = "cross-sectional",
          meanresponse_start = 0.1,
-         meanresponse_end1 = 0.3,
+         meanresponse_end1 = 0.2,
          typeIerror = 0.05, alpha0 = 0.01, alpha1 = 0.01)
 
-## PASS 0.904
-## R shiny 0.907
+## PASS 0.875
+## R shiny 0.875
